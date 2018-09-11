543622f64356d1015248f1a3525c06f878d6f267
EC2 Default User <ec2-user@ip-172-31-42-241.us-east-2.compute.internal>
2018-09-11 17:48:54 +0000

    SE HW 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,70 @@
 
 # Part 1
 
+#intital value is set to 0 and sum operation is carried out on the array. 
+#This is Enumerable type inject.
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)                                                             
 end
 
+#sort the array in asc order and take sum of last 2 elements
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))                                                         
 end
 
+#most ruby to do it would be to use 1.3
+#product gives a cartesian product. Once we get that we can use any? to check all the cartesian pairs which sum up to n. 
+#But this approach will fail for one of the cases where array might have duplicate values.
+#for instance let arr=[1,2,4,4] and n be 8, 1.3  will fail this test, since it will eliminate 4,4 duplet because of c[0] != c[1].
+#which was put in order to consider for e.g. [1,2,4] and 8, where 4,4 might come as true.
+# A better approach would be to use derivate of DNF algorithm to optimize the search time and cover all cases.
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  #arr.product(arr).any? {|c| sum(c) == n && c[0] != c[1] }    ----1.3
+  arr = arr.sort
+  low = 0
+  high = arr.length - 1
+  while low < high
+    if arr[low] + arr[high] == n
+      return true
+    end
+    arr[low] + arr[high] < n ? low += 1 : high -=  1 
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
+#first ^acts as a not, second ^ inside [ acts as a starts with, \W is included for non-word character and finally /i is to make it case insensitive.
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[^aeiou\W]/i
 end
 
+#String should only have 0's and 1's and end in 00 to be a multiple of 4
+#0 is also a multiple of 4
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /(^0|^[10]*00)$/             
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn                                     #getter, setter for isbn
+	attr_accessor :price                                    #getter, setter for price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+		  "Illegal Argument" if isbn.empty?  or price <= 0    #raise an error if isbn is an empty string or price is less than or  equal to zero
+		@isbn = isbn                                          #intialize isbn
+		@price = price                                        #intialize price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)                             #prepend @price with $ and convert it to  2.2f i.e. float with 2  decimal point formt
+	end
+	
 end
